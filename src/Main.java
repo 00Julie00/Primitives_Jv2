ff1898b5081cb5518d6fb4d47c3c327cb2e0621a
Julia Rusanova <elmarenelcorazon@gmail.com>
2022-11-07 21:51:01 +0300

    add src/ and gitignore as tracked

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountAccount = 200; //
+        int refillAmount = 1500;
+        int bonus;
+        int totalAmount;
+
+        if (refillAmount > 1000) {
+            bonus = (refillAmount * 1 / 100);
+            totalAmount = amountAccount + refillAmount + bonus;
+            System.out.println("Зачислено бонусных" + bonus + "руб.");
+            System.out.println("На Вашем балансе" + totalAmount + "руб.");
+        } else {
+            bonus = 0;
+            System.out.println("Бонусные рубли не зачислены. Чтобы получить бонусные рубли поплните баланс на сумму более 1000 руб.");
+        }
+
+
+    }
+}
